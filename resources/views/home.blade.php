a9cb497cba12cc0a1cacb420d0602c5e29287a59
Isox-z <theo.meunier41@gmail.com>
2021-09-18 18:11:54 +0200

    feat: management navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+
+@section('content')
+    <h1 class="text-red-500">Coucou les gens</h1>
+@endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    <title>Upwork</title>
+    <title>Laravel</title>
     <style>
         html {
             scroll-behavior: smooth;
@@ -20,6 +20,39 @@
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
+<header class="mx-auto container f py-5 mb-4">
+    <nav class="flex justify-between items-center">
+        <ul class="flex ">
+            <div>
+                logo
+            </div>
+            <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('Computer') }}</a></li>
+            <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('Component') }}</a></li>
+            <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('Periferic') }}</a></li>
+            <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('Gaming') }}</a></li>
+            <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('Network') }}</a></li>
+        </ul>
+
+        <ul class="flex">
+            @guest
+                <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('login') }}</a></li>
+                <li class="mx-4 hover:text-blue-500"><a href="#">{{ __('register') }}</a></li>
+            @else
+                <li class="mx-4 hover:text-blue-400"><a href="#">{{ __('Me') }}</a></li>
+                <li>
+                    <a class="nav-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+            @endguest
+        </ul>
+    </nav>
+</header>
 <div class="container mx-auto px-5">
     @yield('content')
     @livewireScripts
